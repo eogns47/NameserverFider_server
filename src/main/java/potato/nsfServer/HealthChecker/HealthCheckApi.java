1c3b94ec6ed0d5469a6983a042bc9f692d94ecc3
kangdaehoon <eogns47@konkuk.ac.kr>
2024-02-23 08:07:03 +0900

    ✨feat: 블루/그린 무중단 배포시 헬스체크를 위한 API 추가

---
+++ src/main/java/potato/nsfServer/HealthChecker/HealthCheckApi.java
@@ -0,0 +1,31 @@
+package potato.nsfServer.HealthChecker;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HealthCheckApi {
+    @Value("${server.env}")
+    private String env;
+
+    @GetMapping("/getServerInfo")
+    public ResponseEntity<Map<String, String>> getServerInfo() {
+        Map<String, String> serverInfo = new HashMap<>();
+        serverInfo.put("env:", env);
+        return ResponseEntity.ok(serverInfo);
+    }
+
+    //profile 조회
+    @GetMapping("/env")
+    public String getEnv() {
+        return env;
+    }
+}
