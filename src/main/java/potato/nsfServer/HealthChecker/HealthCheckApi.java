1890c47243f0744c665babfcd270b2a9bf59b1a3
kangdaehoon <eogns47@konkuk.ac.kr>
2024-02-25 23:31:06 +0900

    🛠️fix: healthcheck 의존성 에러 해결

--- src/main/java/potato/nsfServer/HealthChecker/HealthCheckApi.java
+++ src/main/java/potato/nsfServer/HealthChecker/HealthCheckApi.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class HealthCheckApi {
@@ -19,6 +21,7 @@ public class HealthCheckApi {
 
     @GetMapping("/getServerInfo")
     public ResponseEntity<Map<String, String>> getServerInfo() {
+        log.info("서버 정보 출력" + env);
         String env = environment.getProperty("server.env");
         Map<String, String> serverInfo = new HashMap<>();
         serverInfo.put("env:", env);
